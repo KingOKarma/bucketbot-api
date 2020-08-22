9896e6628dc9ba2431bf8bb3656eeb0243f5e5fe
Kaine Simpkins <kainesimpkins782@gmail.com>
2020-08-23 00:50:04 +0100

    update uptime

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ app.get('/', (req, res) => {
     return res.send('Bucketbot API');
 })
 
+
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+totalSeconds %= 86400;
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = Math.floor(totalSeconds % 60);
+let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
+
 app.get('/bot/stats', (req, res) => {
     return res.json({
         inServers: client.guilds.cache.size,
         memberCount: client.users.cache.size,
         supportCount: client.guilds.cache.find(guild => guild.id === "605859550343462912").memberCount,
-        uptime: client.uptime,
+        uptime: uptime,
     })
 })
 
